2d35b0866b03ca4aafd75ac7a70c472272136688
fa03 <fajarmulya2003>
2026-06-12 21:41:50 +0700

    pertemuan17

--- pertemuan-11/pertemuan-11/latihan5.php
+++
@@ -1,9 +0,0 @@
-<?php
- mysql_connect("localhost","root","");
- // mysql_select_db("lat_dbase");
- $hasil=mysql_db_query("lat_dbase”,"select * from tbl_mhs");
- While($data=mysql_fetch_row($hasil))
- {
-   echo "$data[0] $data[1] $data[2]<br>";
- }
-?>
---
+++ pertemuan-17/tujuan.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>Selamat Kamu Berhasil Keluar dari Error</h1>";
+?>
